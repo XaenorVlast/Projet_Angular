f4aec7e568bffe5e4b391b9cb2b11c151cee8080
clement.charabot <clem10fcb@gmail.com>
2023-12-16 16:13:58 +0100

    1ere version de Clement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ContributeComponent } from './contribute/contribute.component';
+import { CountryComponent } from './country/country.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contribute', component: ContributeComponent },
+  { path: 'country', component: CountryComponent },
+  { path: 'discover', component: AboutComponent },
+  // Ajouter d'autres composants//
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { DiscoverComponent } from './discover/discover.component';
+import { ContributeComponent } from './contribute/contribute.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { CountryComponent } from './country/country.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DiscoverComponent,
+    ContributeComponent,
+    ProductsComponent,
+    AboutComponent,
+    FooterComponent,
+    CountryComponent,
   ],
   imports: [
     BrowserModule,
